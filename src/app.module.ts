36de659f8d84d1f983e0b2032d235be20ffe141d
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-08 17:52:49 +0700

    feat(author) : import author contorller, service, module to app module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { JwtService } from '@nestjs/jwt/dist';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 import { AuthService } from './auth/auth.service';
+import { AuthorController } from './author/author.controller';
+import { AuthorService } from './author/author.service';
+import { AuthorModule } from './author/author.module';
 
 
 @Module({
@@ -14,9 +17,10 @@ import { AuthService } from './auth/auth.service';
     CommonModule,
     ConfigModule.forRoot({
       isGlobal: true, // Agar ConfigService bisa diakses di seluruh aplikasi
-    }), 
+    }),
+    AuthorModule, 
   ],
-  controllers: [AppController],
-  providers: [AppService, AuthService,  JwtService],
+  controllers: [AppController, AuthorController],
+  providers: [AppService, AuthService,  JwtService, AuthorService],
 })
 export class AppModule {}
