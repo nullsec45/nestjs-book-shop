7269264674183210188f7852d6425177484f42bd
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-11 23:32:31 +0700

    feat(category) : aadd api category

---
+++ src/category/category.controller.ts
@@ -0,0 +1,81 @@
+import { Controller,Post,HttpCode,Body, ParseIntPipe,Get,Param, Put, Delete, Query, UseGuards, Res} from "@nestjs/common";
+import { CategoryService } from "@/category/category.service";
+import { CategoryResponse, CreateCategoryRequest, SearchCategoryRequest, UpdateCategoryRequest } from "@/model/category.model";
+import { AuthenticatedGuard } from '@/auth/authenticated.guard';
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard';
+import { Response } from 'express'
+
+@UseGuards(AuthenticatedGuard)
+@UseGuards(JwtAuthGuard)
+@Controller('categories')
+export class CategoryController {
+     constructor(
+            private categoryRequest:CategoryService
+        ){}
+    
+        @Post()
+        async create(
+            @Body() request:CreateCategoryRequest
+        ){
+            const result= await this.categoryRequest.create(request);
+            return {
+                data:result
+            }
+        }
+    
+        @Get('/:param')
+        @HttpCode(200)
+        async get(
+            @Res() response:Response,
+            @Param('param') param:string
+        ){
+            const result=await this.categoryRequest.get(param);
+    
+            return response.status(result.statusCode).json(result);
+        }
+    
+        @Put('/:categoryId')
+        @HttpCode(200)
+        async update(
+            @Res() response:Response,
+            @Param('categoryId') categoryId:string,
+            @Body() request:UpdateCategoryRequest
+        ){
+            request.id=categoryId;
+            const result=await this.categoryRequest.update(request);
+         
+            return response.status(result.statusCode).json(result);
+        }
+    
+        @Delete('/:categoryId')
+        @HttpCode(200)
+        async remove(
+            @Res() response:Response,
+            @Param('categoryId') categoryId:string
+        ){
+            const result=await this.categoryRequest.remove(categoryId);
+            
+            return response.status(result.statusCode).json(result);
+        }
+    
+        @Get()
+        @HttpCode(200)
+        async search(
+            @Res() response:Response,
+            @Query('name') name?:string,
+            @Query('page',new ParseIntPipe({optional:true})) page?:number,
+            @Query('size',new ParseIntPipe({optional:true})) size?:number,
+        ){
+            const request:SearchCategoryRequest={
+                name:name,
+                page:page || 1,
+                size:size || 10
+            }
+    
+            const result=await this.categoryRequest.search(request);
+    
+            
+            return response.status(result.statusCode).json(result);
+    
+        }
+}
