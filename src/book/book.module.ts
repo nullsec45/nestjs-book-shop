10ae1eeb1bcfa7f6cc0d3b628d5fe156c19921ff
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-16 16:18:45 +0700

    feat(book) : export book service

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { BookService } from '@/book/book.service';
+import { BookAuthorModule } from '@/book-author/book-author.module';
 
-@Module({})
+@Module({
+  // imports:[forwardRef(() => BookAuthorModule)],
+  providers: [BookService],
+  exports: [BookService],
+})
 export class BookModule {}
