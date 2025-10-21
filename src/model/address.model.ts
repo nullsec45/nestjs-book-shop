c97bf6fa4a3609ab1adecd10f70ed1dbf3b45b3a
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-21 21:07:31 +0700

    feat(address) : address model.

---
+++ src/model/address.model.ts
@@ -0,0 +1,41 @@
+export class CreateAddressRequest {
+    user_id:string;
+    label:string;
+    recipient_name:string;
+    phone: string;
+    line:string;
+    city:string;
+    province:string;
+    is_default:boolean;
+}
+
+export class AddressResponse {
+    id:string;
+    user_id:string;
+    label:string;
+    recipient_name:string;
+    phone: string;
+    line:string;
+    city:string;
+    province:string;
+    is_default:boolean;
+}
+
+export class UpdateAddressRequest {
+    id:string;
+    user_id:string;
+    label:string;
+    recipient_name:string;
+    phone: string;
+    line:string;
+    city:string;
+    province:string;
+    is_default:boolean;
+}
+
+
+export class SearchAddressRequest{
+    title?:string;
+    page:number;
+    size:number;
+}
