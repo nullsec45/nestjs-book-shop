1203f2b2777760a17f48a0a374a5fbb5afab8c9c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-22 17:30:54 +0700

    feat(auth) : rename from user.validation to auth.validation.

--- src/auth/auth.validation.ts
+++ src/auth/auth.validation.ts
@@ -14,12 +14,4 @@ export class UserValidation {
     email: z.email(),
     password: z.string().min(1),
   });
-
-  static readonly UPDATE = z.object({
-    name:z.string().min(5).max(255),
-    username:z.string().min(5).max(100),
-    email: z.email(),
-    password: z.string().min(8).max(100),
-    role: z.enum(["ADMIN","CUSTOMER"]),
-  });
 }
