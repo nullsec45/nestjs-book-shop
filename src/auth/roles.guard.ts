6b4e264d20b31d3371c9d6fe6ba35732a46ed0a3
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-10-25 23:01:58 +0700

    feat(auth) : role guard

---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,41 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    ForbiddenException
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserService } from '@/user/user.service';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from '@/auth/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate{
+    constructor(
+        private reflector:Reflector,
+        private userService:UserService,
+    ){}
+
+    async canActivate(ctx: ExecutionContext): Promise<boolean> {
+        const required=this.reflector.getAllAndOverride<Role[]>(ROLES_KEY,[
+            ctx.getHandler(),
+            ctx.getClass(),
+        ]);
+
+        if (!required || required.length == 0) return true;
+
+        const req=ctx.switchToHttp().getRequest();
+        const user=req.user;
+        console.log(user);
+        if(!user?.id){
+            throw new ForbiddenException('Unauthorized');
+        }
+
+        const userRole=await this.userService.getRoleByUserId(user.id);
+
+        const allowed=required.includes(userRole as Role);
+
+        if(!allowed) throw new ForbiddenException('Insufficient role');
+        return true;
+    }
+}
