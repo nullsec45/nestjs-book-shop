209dede4a107141524478d865405ec34f83d7568
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-01 14:30:06 +0700

    feat(user) : import media module.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserService } from '@/user/user.service';
+import { MediaModule } from '@/media/media.module';
 
 @Module({
+    imports:[
+      forwardRef(() => MediaModule)  
+    ],
     providers: [UserService],
     exports: [UserService],
 })
