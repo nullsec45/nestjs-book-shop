7d528f3751ec00eeb33a85cb6b0f4356afcc504c
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-01 14:30:55 +0700

    feat(media) : import user module.

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,28 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
-import { diskStorage, memoryStorage } from 'multer';
-import * as fs from 'fs';
-import * as path from 'path';
-import { Logger, forwardRef } from '@nestjs/common';
+import {  memoryStorage } from 'multer';
+import {  forwardRef } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FileUploadService } from '@/common/file-upload.service';
 import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 import { BookModule } from '@/book/book.module';
-import { randomFileName } from '@/utils/fileName';
+import { UserModule } from '@/user/user.module';
 
 @Module({
     imports: [
         forwardRef(() => BookModule),
-         MulterModule.registerAsync({
+        forwardRef(() => UserModule),
+        MulterModule.registerAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: () => ({
                 storage:memoryStorage(),
             }),
         }),
-        
     ],
     controllers: [MediaController],
     providers: [MediaService,FileUploadService],
