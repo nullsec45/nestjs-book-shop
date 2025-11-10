c33bd82cfa7c0a764450f64de9532961715f9cb8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-10 23:20:07 +0700

    feat(book) : remove unused in module.

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,6 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { BookService } from '@/book/book.service';
-import { BookAuthorModule } from '@/book-author/book-author.module';
 import { MediaModule } from '@/media/media.module';
 import { BookController } from './book.controller';
 import { UserModule } from '@/user/user.module';
