93baf5945216eaeb77499e48933e08653536f839
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-11-10 23:19:10 +0700

    feat(logger) : add log in roles guard.

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -8,12 +8,14 @@ import { Reflector } from '@nestjs/core';
 import { UserService } from '@/user/user.service';
 import { ROLES_KEY } from './roles.decorator';
 import { Role } from '@/auth/role.enum';
+import { LogService } from '@/common/logger.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate{
     constructor(
         private reflector:Reflector,
         private userService:UserService,
+        private logger:LogService,
     ){}
 
     async canActivate(ctx: ExecutionContext): Promise<boolean> {
@@ -34,6 +36,13 @@ export class RolesGuard implements CanActivate{
 
         const allowed=required.includes(userRole as Role);
 
+        this.logger.module('activity').warn('forbidden.access', {
+            url: req.url,
+            method: req.method,
+            reason: 'Insufficient role',
+            user_id:req.user.id,
+        },'warn');
+        
         if(!allowed) throw new ForbiddenException('Insufficient role');
         return true;
     }
